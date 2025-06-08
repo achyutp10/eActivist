fdfd207774f6ecd8be355b0b23d08eddcc0de1ed
Achyut <achyutp10@gmail.com>
2025-06-08 12:17:08 +0545

    RDN

--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import {
   Button,
   Card,
@@ -7,11 +7,14 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 export default function ActivityDetails() {
   const navigate = useNavigate();
-  const activity = {} as Activity;
-  if (!activity) return <Typography>Loading...</Typography>;
+  const { id } = useParams();
+  const { activity, isLoadingActivity } = useActivities(id);
+  if (isLoadingActivity) return <Typography>Loading...</Typography>;
+  if (!activity) return <Typography>Activity Not Found...</Typography>;
 
   return (
     <Card sx={{ borderRadius: 3 }}>
@@ -27,11 +30,7 @@ export default function ActivityDetails() {
         <Typography variant="body1">{activity.description}</Typography>
       </CardContent>
       <CardActions>
-        <Button
-          component={Link}
-          to={`/activities/${activity.id}`}
-          color="primary"
-        >
+        <Button component={Link} to={`/manage/${activity.id}`} color="primary">
           Edit
         </Button>
         <Button color="inherit" onClick={() => navigate("/activities")}>
