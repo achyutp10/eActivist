d375102a36d995e5c883b5d58fcde38a27e613c7
Achyut <achyutp10@gmail.com>
2025-06-08 07:23:57 +0545

    RDN

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,11 +7,10 @@ import {
   // ListItem,
   // ListItemText,
 } from "@mui/material";
-import axios from "axios";
 import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useQuery } from "@tanstack/react-query";
+import { useActivities } from "../../lib/hooks/useActivities";
 
 function App() {
   const [selectedActivity, setSelectedActivity] = useState<
@@ -19,15 +18,7 @@ function App() {
   >(undefined);
   const [editMode, setEditMode] = useState(false);
 
-  const { data: activities, isPending } = useQuery({
-    queryKey: ["activities"],
-    queryFn: async () => {
-      const response = await axios.get<Activity[]>(
-        "https://localhost:5001/api/activities"
-      );
-      return response.data;
-    },
-  });
+  const { activities, isPending } = useActivities();
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities!.find((x) => x.id === id));
