42f2c90712564f271bd4724b6d27774ac45833b0
Achyut <achyutp10@gmail.com>
2025-06-09 14:08:11 +0545

    RDN

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { store } from "../stores/store";
+import { toast } from "react-toastify";
+import { router } from "../../app/router/routes";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -16,16 +18,50 @@ agent.interceptors.request.use((config) => {
   return config;
 });
 
-agent.interceptors.response.use(async (response) => {
-  try {
-    await sleep(1000);
-    return response;
-  } catch (error) {
-    console.log(error);
-    return Promise.reject(error);
-  } finally {
+agent.interceptors.response.use(
+  async (response) => {
+    if (import.meta.env.DEV) await sleep(1000);
     store.uiStore.isIdle();
+    return response;
+  },
+  async (error) => {
+    if (import.meta.env.DEV) await sleep(1000);
+    store.uiStore.isIdle();
+
+    const { status, data } = error.response;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        if (data.detail === "NotAllowed") {
+          throw new Error(data.detail);
+        } else {
+          toast.error("Unauthorised");
+        }
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        router.navigate("/server-error", { state: { error: data } });
+        break;
+      default:
+        break;
+    }
+
+    return Promise.reject(error);
   }
-});
+);
 
 export default agent;
